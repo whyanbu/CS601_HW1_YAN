d4a61c1ab664b4c0ff29dd532b9150d5cd03d80a
whyanbu <whyan@bu.edu>
2025-11-02 22:39:20 +0800

    added regex check for email address

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A webpage for register a subscription of newsletter by providing basic personal 
 Features  
 Installation  
 Usage  
-Project Structure  
+Project Structure
+Email Address Validation
 
 ### Features
 Written with vanilla JavaScript without CSS frameworks.
@@ -34,7 +35,23 @@ CS601_HW1_YAN/
 │   └── main.js  
 ├── css/  
 │   └── main.css  
-├── index.html  
+├── index.html
+├── .gitignore  
 └── README.md  
 ```
 index.html: Entry point for the app.  
+
+### Email Address Validation
+Regular Expression is used to validate the input email address.  The expression used is:
+^[a-zA-Z]+[a-zA-Z0-9\.]*[a-zA-Z0-9]+@[a-zA-Z]+[\.][a-zA-Z]+
+
+The validation rules are listed as follows:
+Usernmae should start with a-z or A-Z
+Username should end with a-z or A-Z or 0-9
+Dot "." is allowed in username but not at the beginning or end
+Username should contains at least 2 characters
+Symbol "@" must be present exactly once
+Username must be immediately before the @ symbol
+Symbol "@" must be followed by a domain name
+Domain name should be at least one letter of a-z or A-Z then followed by a dot "." and followed by at least one letter of a-z or A-Z
+numbers not allowed in domain name
--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ const lastName = document.getElementById("input-last-name");
 const email = document.getElementById("input-email");
 const package = document.getElementById("input-package");
 const subscription = document.getElementById("input-subscription");
+const lblSubcription = document.querySelector("label[for=input-subscription]");
 const submit = document.getElementById("btn-submit");
 const errorMessages = document.getElementById("error-messages");
 const successMessages = document.getElementById("success-messages");
@@ -41,30 +42,41 @@ const containsIllegalCharacters = input => {
 submit.addEventListener("click", e => {
     e.preventDefault();     // prevent page reload
     const messages = [];
+    firstName.classList.remove("error-input");
+    lastName.classList.remove("error-input");
+    email.classList.remove("error-input");
+    package.classList.remove("error-input");
+    lblSubcription.classList.remove("error-text");
     if (getNumOfAlphaCharacters(firstName.value.trim()) < 2) {
         messages.push("First name should contains at least 2 alpha characters");
+        firstName.classList.add("error-input");
     }
     if (containsIllegalCharacters(firstName.value.trim())) {
         messages.push("First name contains illegal characters");
+        firstName.classList.add("error-input");
     }
     if (getNumOfAlphaCharacters(lastName.value.trim()) < 2) {
         messages.push("Last name should contains at least 2 alpha characters");
+        lastName.classList.add("error-input");
     }
     if (containsIllegalCharacters(lastName.value.trim())) {
         messages.push("Last name contains illegal characters");
+        lastName.classList.add("error-input");
     }
     if (email.value.trim() === "") {
         messages.push("Email address is required");
-    }
-    email.reportValidity();
-    if (!email.checkValidity()) {
+        email.classList.add("error-input");
+    } else if (!email.value.match(/^[a-zA-Z]+[a-zA-Z0-9\.]*[a-zA-Z0-9]+@[a-zA-Z]+[\.][a-zA-Z]+/)) {
         messages.push("Email address format not correct");
+        email.classList.add("error-input");
     }
     if (!packageOptions.includes(package.value)) {
         messages.push("A package must be selected");
+        package.classList.add("error-input");
     }
     if (subscription.checked === false) {
         messages.push("Please confirm subscription to newsletter");
+        lblSubcription.style.color = "red";
     }
     if (messages.length > 0) {
         errorMessages.style.display = "block";
